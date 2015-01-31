79c7665320ae068832dd63dcdec7db3b890fdcae
Jonathan Edwards <apense+git@gmail.com>
2015-01-31 12:42:46 -0500

    Formatting changes for easier reading

--- src/vecmath.nim
+++ src/vecmath.nim
@@ -23,13 +23,17 @@ import math
 import strutils
 import unsigned
 import private/intrinsic
+
 type ColMajor = object
 type RowMajor = object
 type Options = generic x
   x is ColMajor or
     x is RowMajor
-type TMatrix*[N: static[int]; M: static[int]; T; O: Options] = object
-  data*: array[0..M*N-1, T]
+
+type 
+  TMatrix*[N: static[int]; M: static[int]; T; O: Options] = object
+    data*: array[0..M*N-1, T]
+
 type 
   SquareMatrix[N: static[int]; T] = TMatrix[N,N,T,ColMajor]
 
@@ -50,10 +54,12 @@ type
   TVec3f* = TMatrix[3, 1, float32, ColMajor]
   TVec2f* = TMatrix[2, 1, float32, ColMajor]
   TQuatf* = distinct array[1..4, float32] #poor man's quaternion
+
 type
   TAlignedBox3f* = object
     min*: TVec3f
     max*: TVec3f
+
 type TCornerType* = enum
   ctBottomLeftFloor = 0,
   ctBottomRightFloor = 1,
@@ -63,13 +69,16 @@ type TCornerType* = enum
   ctBottomRightCeil = 5,
   ctTopLeftCeil = 6,
   ctTopRightCeil = 7
+
 type TAxis* = enum
   axisYZ = 1,
   axisXZ = 2,
   axisXY = 3
+
 type TRay* = object
   dir*: TVec3f
   origin*: TVec3f
+
 proc `[]=`*(self: var TMatrix; i,j: int; val: TMatrix.T) =
   when TMatrix.O is RowMajor:
     var idx = (TMatrix.M * (i-1)) + (j-1)
@@ -85,53 +94,68 @@ proc `[]`*(self: TMatrix; i,j: int): TMatrix.T =
   when TMatrix.O is ColMajor:
     var idx = (TMatrix.N * (j-1)) + (i-1)
     result = self.data[idx]
+
 proc `[]`*(self: TVec; i: int): TVec.T =
   result = self[i, 1]
+
 proc `[]=`*(self: var TVec; i: int; val: TVec.T) =
   self[i, 1] = val
 
 proc vec2f*(x,y: float32): TVec2f =
   result.data = [x,y]
+
 proc vec3f*(x,y,z: float32): TVec3f =
   result.data = [x,y,z]
+
 proc vec3f*(vec: TVec4f): TVec3f =
   result[1] = vec[1]
   result[2] = vec[2]
   result[3] = vec[3]
+
 proc vec4f*(x,y,z,w: float32): TVec4f =
   result.data = [x,y,z,w]
+
 proc vec4f*(v: TVec3f, w: float32): TVec4f =
   result.data = [v[1], v[2], v[3], w]
+
 proc vec4*[T](x,y,z,w: T): TVec4[T] =
   result.data = [x,y,z,w]
+
 proc vec4*[T](v: TVec3[T], w: T): TVec4[T] =
   result.data = [v[1], v[2], v[3], w]
 
 
 proc rows*(mtx: TMatrix): int = TMatrix.N
 proc cols*(mtx: TMatrix): int = TMatrix.M
+
 proc identity*[T](): T =
   for i in 1..rows(result):
     result[i,i] = 1.float32
+
 proc dot*(a, b: TVec): TVec.T =
   #FIXME: perhaps this should return a float
   #assert(a.data.len == b.data.len)
   for i in 1..TVec.N:
     result += a[i] * b[i]
+
 proc length*(a: TVec): float =
   result = sqrt(dot(a,a).float)
+
 proc row*(a: TMatrix; i: int): auto =
   result = TVec[TMatrix.M, TMatrix.T]()
   for idx in 1..TMatrix.M:
     result[idx] = a[i,idx]
+
 proc col*(a: TMatrix; j: int): auto =
   result = TVec[TMatrix.N, TMatrix.T]()
   for idx in 1..TMatrix.N:
     result[idx] = a[idx, j]
+
 proc `/`*(a: TMatrix, c: float): TMatrix =
   for i in 1..TMatrix.N:
     for j in 1..TMatrix.M:
       result[i,j] = a[i,j] / c
+
 proc sub*(self: TMatrix; r,c: int): auto =
   ## returns a submatrix of `self`, that is
   ## we delete the ith row and jth column
@@ -146,6 +170,7 @@ proc sub*(self: TMatrix; r,c: int): auto =
       elif i >= r: result[i,j] = self[i+1, j]
       elif j >= c: result[i,j] = self[i, j+1]
       else: result[i,j] = self[i,j]
+
 proc transpose*(a: TMatrix): TMatrix =
   for i in 1..TMatrix.N:
     for j in 1..TMatrix.M:
@@ -172,40 +197,52 @@ proc adj*(a: TMat4f): TMat4f =
     for j in 1..4:
       var sgn = pow((-1).float, (i+j).float)
       result[i,j] = sgn * det(a.sub(j,i))
+
 proc inverse*(a: TMat4f): TMat4f =
   result = adj(a)/det(a)
+
 proc trace*(a: SquareMatrix): float =
   for i in 1..SquareMatrix.N:
     result += a[i,i]
+
 proc initMat3f*(arrs: array[0..8, float32]): TMat3f = 
   result.data = arrs
   result = transpose(result)
+
 proc initMat2f*(arrs: array[0..3, float32]): TMat2f =
   result.data = arrs
   result = transpose(result)
+
 proc mat3f*(mat: TMat4f): TMat3f =
   for i in 1..3:
     for j in 1..3:
       result[i,j] = mat[i,j]
+
 proc `$`*(a: TMat3f): string =
   result = formatFloat(a[1,1]) & " " & formatFloat(a[1,2]) & formatFloat(a[1,3]) & "\n" &
            formatFloat(a[2,1]) & " " & formatFloat(a[2,2]) & formatFloat(a[2,3]) & "\n" &
            formatFloat(a[3,1]) & " " & formatFloat(a[3,2]) & formatFloat(a[3,3])
+
 proc mul*(a: TMat4f; b: TMat4f): TMat4f =
   for i in 1..4:
     for j in 1..4:
       result[i,j] = dot(row(a,i), col(b,j))
+
 proc mulv*(a: TMat3f, b: TVec3f): TVec3f =
   for i in 1..3:
     result[i] = dot(a.row(i), b)
+
 proc mul4v*(a: TMat4f, v: TVec4f): TVec4f =
   for i in 1..4:
     result[i] = dot(a.row(i), v)
+
 proc mul3v*(a: TMat4f, b: TVec3f): TVec3f =
   result = vec3f(mul4v(a, vec4f(b, 1)))
+
 proc mul3vd*(a: TMat4f, b: TVec3f): TVec3f =
   ## treats the input as a direction, not as a position
   result = vec3f(mul4v(a, vec4f(b, 0)))
+
 discard """
 proc mul*(a: TMat3f; b: TMat3f): TMat3f =
   for i in 1..3:
@@ -214,12 +251,15 @@ proc mul*(a: TMat3f; b: TMat3f): TMat3f =
 """
 #proc `==`*(a: TMatrix; b: TMatrix): bool =
 #  result = a.data == b.data
+
 proc identity4f*(): TMat4f =
   for i in 1..4:
     result[i,i] = 1'f32
+
 proc identity3f*(): TMat3f =
   for i in 1..3:
     result[i,i] = 1'f32
+
 #vector only code
 proc x*(a: TVec): TVec.T = a[1]
 proc y*(a: TVec): TVec.T = a[2]
@@ -230,50 +270,65 @@ proc `y=`*(a: var TVec, val: TVec.T) = a[2] = val
 proc `z=`*(a: var TVec, val: TVec.T) = a[3] = val
 proc `w=`*(a: var TVec, val: TVec.T) = a[4] = val
 proc xyz*(a: TVec4f): TVec3f = vec3f(a.x, a.y, a.z)
+
 proc norm*(a: TVec): float =
   sqrt(dot(a,a))
+
 proc normalize*(a: TVec): TVec =
   result = a / norm(a)
+
 proc `+`*(a, b: TVec): TVec =
   for i in 1..TVec.N:
     result[i] = a[i] + b[i]
+
 proc `+=`*(a: var TVec, b: TVec) =
   a = a+b
+
 proc `-`*(a, b: TVec): TVec =
   for i in 1..TVec.N:
     result[i] = a[i] - b[i]
+
 proc `-`*(a: TVec, c: float): TVec =
   for i in 1..TVec.N:
     result[i] = a[i] - c
+
 proc `*`*(a: TVec, b: float): TVec =
   for i in 1..TVec.N:
     result[i] = a[i] * b
+
 proc `*`*(b: float, a: TVec): TVec = a * b
+
 proc `<`*(a: TVec, b: TVec): bool =
   result = true
   for i in 1..TVec.N:
     if a[i] >= b[i]:
       return false
+
 proc `<=`*(a: TVec, b: TVec): bool =
   result = true
   for i in 1..TVec.N:
     if a[i] > b[i]:
       return false
+
 proc dist*(a,b: TVec): float =
   result = norm(a - b)
+
 proc formatVec3f*(a: TVec3f): string {.noSideEffect.} =
   result  =  "x: " & formatFloat(a[1])
   result &= " y: " & formatFloat(a[2])
   result &= " z: " & formatFloat(a[3])
+
 proc formatVec4f*(a: TVec4f): string {.noSideEffect.} =
   result  =  "x: " & formatFloat(a[1])
   result &= " y: " & formatFloat(a[2])
   result &= " z: " & formatFloat(a[3])
   result &= " w: " & formatFloat(a[4])
+
 proc cross*(u,v: TVec3f): TVec3f =
   result.x = (u.y * v.z) - (u.z * v.y)
   result.y = (u.z * v.x) - (u.x * v.z)
   result.z = (u.x * v.y) - (u.y * v.x)
+
 proc extrema*(vecs: varargs[TVec3f]): tuple[min,max: TVec3f] =
   result.min.x = Inf
   result.min.y = Inf
@@ -296,19 +351,23 @@ proc toAffine*(a: TMat3f): TMat4f =
     for j in 1..TMat3f.M:
       result[i,j] = a[i,j]
   result[4,4] = 1'f32
+
 proc fromAffine*(a: TMat4f): TMat3f =
   for i in 1..3:
     for j in 1..3:
       result[i,j] = a[i,j]
+
 proc toTranslationMatrix*(v: TVec3f): TMat4f =
   result = identity4f()
   result[1,4] = v[1]
   result[2,4] = v[2]
   result[3,4] = v[3]
+
 proc fromTranslationMtx*(m: TMat4f): TVec3f =
   result[1] = m[1,4]
   result[2] = m[2,4]
   result[3] = m[3,4]
+
 proc unProject*(win: TVec3f; mtx: TMat4f, viewport: TVec4f): TVec3f =
   var inversevp = inverse(mtx)
   var tmp = vec4f(win, 1'f32)
@@ -318,8 +377,10 @@ proc unProject*(win: TVec3f; mtx: TMat4f, viewport: TVec4f): TVec3f =
   var obj = mul4v(inversevp, tmp)
   obj = obj / obj[4]
   result = vec3f(obj[1], obj[2], obj[3])
+
 proc unProject*(win: TVec3f; view, proj: TMat4f; viewport: TVec4f): TVec3f =
   unProject(win, mul(proj, view), viewport)
+
 #projection related code
 proc CreateOrthoMatrix*(min, max: TVec3f): TMat4f =
   var sx = 2 / (max.x - min.x)
@@ -332,8 +393,10 @@ proc CreateOrthoMatrix*(min, max: TVec3f): TMat4f =
                  0,  sy,  0,    0,
                  0,  0,   sz,   0,
                  tx,  ty, tz,   1]
+
 proc CreateOrthoMatrix*(box: TAlignedBox3f): TMat4f =
   result = CreateOrthoMatrix(box.min, box.max)
+
 proc CreateOrthoMatrix*(left, right, bottom, top, near, far: float32): TMat4f =
   ## this works just like glOrtho
   result.data = [2/(right - left), 0, 0, 0,
@@ -341,6 +404,7 @@ proc CreateOrthoMatrix*(left, right, bottom, top, near, far: float32): TMat4f =
                  0, 0, -2 / (far - near), 0,
                  -(right + left)/(right - left), -(top+bottom)/(top-bottom), 
                  -2 * ((far + near)/(far - near)), 1]
+
 #quaternion related code
 proc `[]`*(self: TQuatf; i: int): float32 = array[1..4, float32](self)[i]
 proc `[]=`*(self: var TQuatf; i: int; val: float32) = array[1..4,float32](self)[i] = val
@@ -357,25 +421,34 @@ proc x*(q: TQuatf): float32 = q[2]
 proc y*(q: TQuatf): float32 = q[3]
 proc z*(q: TQuatf): float32 = q[4]
 proc mul*(p: TQuatf, q: TQuatf): TQuatf
+
 proc `/`*(q: TQuatf, s: float): TQuatf =
   result.i = q.i / s
   result.j = q.j / s
   result.k = q.k / s
   result.w = q.w / s
+
 proc `/`*(q: TQuatf, s: float32): TQuatf = q / s.float
+
 proc quatf*(w,i,j,k: float32): TQuatf = [w,i,j,k].TQuatf
+
 proc toVector(q: TQuatf): TVec4f =
   result.data = array[0..3, float32](q)
+
 proc norm*(q: TQuatf): float = norm(toVector(q))
+
 proc normalize*(q: TQuatf): TQuatf = q / norm(q)
+
 proc conj*(q: TQuatf): TQuatf =
   result.i = -q.i
   result.j = -q.j
   result.k = -q.k
   result.w = q.w
+
 proc inverse*(q: TQuatf): TQuatf =
   result = conj(q)
   result = result / mul(q, conj(q)).w
+
 proc mul*(p: TQuatf; q: TQuatf): TQuatf =
   result[1] = p.w * q.w - p.x * q.x - p.y * q.y - p.z * q.z;
   result[2] = p.w * q.x + p.x * q.w + p.y * q.z - p.z * q.y;
@@ -386,12 +459,14 @@ proc mulv*(q: TQuatf; v: TVec3f): TVec3f =
   var qv = quatf(0, v.x, v.y, v.z)
   var prod = mul(q, mul(qv, conj(q)))
   result = vec3f(prod.i, prod.j, prod.k)
+
 proc mul*(p: TQuatf; a: TAlignedBox3f): TAlignedBox3f =
   var rotmin = mulv(p, a.min)
   var rotmax = mulv(p, a.max)
   for i in 1..3:
     result.min[i] = min(a.min[i], rotmin[i])
     result.max[i] = max(a.max[i], rotmax[i])
+
 proc toRotMatrix*(q: TQuatf): TMat3f =
   #this code is ported from Eigen
   #pretty much directly
@@ -419,6 +494,7 @@ proc toRotMatrix*(q: TQuatf): TMat3f =
   result[3,1] = txz-twy
   result[3,2] = tyz+twx
   result[3,3] = float32(1)-(txx+tyy)
+
 proc fromRotMatrix*(m: TMat3f): TQuatf =
   ## convert a rotation matrix into a quaternion
   ## this algorithm comes from the Eigen linear algebra
@@ -447,16 +523,19 @@ proc fromRotMatrix*(m: TMat3f): TQuatf =
     result.w = (m[k,j] - m[j,k]) * t
     result[j] = (m[j,i] + m[i,j]) * t
     result[k] = (m[k,i] + m[i,k]) * t
+
 proc toOrbitRotMatrix*(q: TQuatf, pos: TVec3f): TMat4f =
   result = toTranslationMatrix(-1 * pos)
   result = mul(result, toAffine(toRotMatrix(q)))
   result = mul(result, toTranslationMatrix(pos))
+
 proc quatFromAngleAxis*(angle: float; axis: TVec3f): TQuatf =
   var vecScale = sin(0.5 * angle)
   result[2] = axis[1] * vecScale
   result[3] = axis[2] * vecScale
   result[4] = axis[3] * vecScale
   result[1] = cos(0.5 * angle)
+
 proc quatFromTwoVectors*(v,w: TVec3f): TQuatf =
   var v = normalize(v)
   var w = normalize(w)
@@ -465,6 +544,7 @@ proc quatFromTwoVectors*(v,w: TVec3f): TQuatf =
   result.j = c.y
   result.k = c.z
   result.w = 1 + dot(v,w)
+
 proc identityQuatf*(): TQuatf =
   result[1] = 1.0'f32
   result[2] = 0.0'f32
@@ -478,25 +558,30 @@ proc contains*(aabb: TAlignedBox3f, target: TAlignedBox3f): bool =
   if (target.max <= aabb.max) and (target.max >= aabb.min):
     return true
   return false
+
 proc encloses*(aabb: TAlignedBox3f, target: TAlignedBox3f): bool =
   if (target.min >= aabb.min) and (target.max <= aabb.max):
     return true
   return false
+
 proc extend*(aabb: var TAlignedBox3f, target: TVec3f) =
   for i in 1..3:
     if target[i] < aabb.min[i]: aabb.min[i] = target[i]
     if target[i] > aabb.max[i]: aabb.max[i] = target[i]
+
 proc extend*(aabb: var TAlignedBox3f, target: TAlignedBox3f) =
   for i in 1..3:
     if target.min[i] < aabb.min[i]: aabb.min[i] = target.min[i]
     if target.max[i] > aabb.max[i]: aabb.max[i] = target.max[i]
   #assert(target in aabb)
+
 proc corner*(aabb: TAlignedBox3f, which: TCornerType): TVec3f =
   var mult = 1.uint
   for i in 1..3:
     if (mult and which.uint) > 0.uint: result[i] = aabb.max[i]
     else: result[i] = aabb.min[i]
     mult = mult * 2
+
 proc split*(aabb: TAlignedBox3f, axis: TAxis): tuple[a,b: TAlignedBox3f] =
   result.a = aabb
   result.b = aabb
@@ -504,10 +589,12 @@ proc split*(aabb: TAlignedBox3f, axis: TAxis): tuple[a,b: TAlignedBox3f] =
   var diff = aabb.max - aabb.min
   result.a.min[axisIdx] = result.a.min[axisIdx] + (diff[axisIdx] / 2)
   result.b.max[axisIdx] = result.b.max[axisIdx] - (diff[axisIdx] / 2)
+
 proc split*(aabb: TAlignedBox3f, axis: TAxis; a,b: var TAlignedBox3f) =
   var (tmpa, tmpb) = split(aabb, axis)
   a = tmpa
   b = tmpb
+
 proc split*(aabb: TAlignedBox3f): array[1..8, TAlignedBox3f] =
   split(aabb, axisYZ, result[1], result[3])
   split(result[1], axisXZ, result[1], result[2])
@@ -516,8 +603,10 @@ proc split*(aabb: TAlignedBox3f): array[1..8, TAlignedBox3f] =
   split(result[2], axisXY, result[2], result[6])
   split(result[3], axisXY, result[3], result[7])
   split(result[4], axisXY, result[4], result[8])
+
 proc centroid*(aabb: TAlignedBox3f): TVec3f =
   result = (aabb.min + aabb.max) / 2
+
 proc mulArea*(aabb: TAlignedBox3f, mat: TMat4f): TAlignedBox3f =
   ## multiplies the aabb by the matrix and preserves the area
   ## this means that it is not "real" matrix multiplication, but
@@ -530,6 +619,7 @@ proc mulArea*(aabb: TAlignedBox3f, mat: TMat4f): TAlignedBox3f =
   var minmax = extrema(transformedPoints)
   result.min = minmax.min
   result.max = minmax.max
+
 proc `$`*(aabb: TAlignedBox3f): string {.noSideEffect.} =
   result = "min: " & formatVec3f(aabb.min) & "\nmax: " & formatVec3f(aabb.max)
 
@@ -545,6 +635,7 @@ type TIntersectRay* = object
   ii,ij,ik:float
   ibyj,jbyi,kbyj,jbyk,ibyk,kbyi: float
   c_xy,c_yx,c_zy,c_yz,c_xz,c_zx: float
+
 proc Precompute_Ray*(ray: TRay): TIntersectRay =
   const XXX = {MMM,MMP,MPM,MPP,PMM,PMP,PPM,PPP,POO,MOO,OPO,OMO,OOP,OOM,
                OMM,OMP,OPM,OPP,MOM,MOP,POM,POP,MMO,MPO,PMO,PPO}
@@ -578,24 +669,33 @@ proc Precompute_Ray*(ray: TRay): TIntersectRay =
   result.c_yz = result.z - result.kbyj * result.y
   result.c_xz = result.z - result.ibyk * result.x
   result.c_zx = result.x - result.jbyk * result.z
+  
   var possibleClass: set[TRayClassification] = XXX
   #the paper does this with if statements, but I like this way much better
+  
   if result.i < 0: possibleClass = possibleClass * MXX
   elif result.i == 0: possibleClass = possibleClass * OXX
   else: possibleClass = possibleClass * PXX
+  
   if result.j < 0: possibleClass = possibleClass * XMX
   elif result.j == 0: possibleClass = possibleClass * XOX
   else: possibleClass = possibleClass * XPX
+  
   if result.k < 0: possibleClass = possibleClass * XXM
   elif result.k == 0: possibleClass = possibleClass * XXO
   else: possibleClass = possibleClass * XXP
+  
   assert(card(possibleClass) == 1)
   var intVal = cast[int64](possibleClass)
   var idx: uint32 = 0
+  
   BitScanForward64(addr idx, intVal)
   result.class = cast[TRayClassification](idx)
 
+
 proc intersects*(r: TIntersectRay, b: TAlignedBox3f): bool =
+  result = false
+
   case r.class
   of MMM:
     if (r.x < b.min.x) or (r.y < b.min.y) or (r.z < b.min.z) or 
@@ -771,19 +871,22 @@ proc intersects*(r: TIntersectRay, b: TAlignedBox3f): bool =
       (r.x < b.min.x) or (r.x > b.max.x) or
       (r.y < b.min.y) or (r.y > b.max.y): return false
     return true
-  return false
+
 proc intersects*(r: TRay, b: TAlignedBox3f): bool =
   result = intersects(Precompute_Ray(r), b)
+
 # frustum related code, for culling and
 # other stuff
 type TPlane* = distinct TVec4f
 type TNormalPlane* = distinct TVec4f
+
 proc extractPlane*(matrix: TMat4f, side, sign: int): TPlane =
   ## extract a frustum plane from a matrix
   ## the side and sign parameters determine
   ## which row vector is used and weather it is added
   ## or subtracted from the last row vector
   result = TPlane(matrix.row(4) + float(sign)*matrix.row(side))
+
 proc extractPlane*(matrix: TMat4f, plane: int): TPlane =
   assert(plane > 0)
   assert(plane < 7)
@@ -791,16 +894,19 @@ proc extractPlane*(matrix: TMat4f, plane: int): TPlane =
   if sgn > 0: sgn = -1
   else: sgn = 1
   result = extractPlane(matrix, (plane div 2) + 1, sgn)
+
 proc toHessianNormalForm*(plane: TPlane): TNormalPlane =
   var asq = pow(TVec4f(plane).x, 2)
   var bsq = pow(TVec4f(plane).y, 2)
   var csq = pow(TVec4f(plane).z, 2)
   var denom = sqrt(asq + bsq + csq)
   result = (plane.TVec4f / denom).TNormalPlane
+
 proc distance(plane: TNormalPlane, point: TVec3f): float =
   var plane = plane.TVec4f
   var normVec = vec3f(plane.x, plane.y, plane.z)
   result = dot(normVec, point) + plane.w
+
 proc frustumContains*(frustum: TMat4f, box: TAlignedBox3f): bool =
   result = true
   for i in 1..6:
@@ -858,6 +964,8 @@ proc LookAt*(eye, center, up: TVec3f): TMat4f =
   result = result.transpose()
   var eyeTrans = toTranslationMatrix(-1 * eye)
   result = mul(eyeTrans, result)
+
+
 when isMainModule:
 
   import unittest
@@ -866,16 +974,19 @@ when isMainModule:
     var av = vec3f(0,0,0)
     var bv = vec3f(1,1,1)
     check(av < bv)
+  
   test "TNotLessThan":
     var av = vec3f(1,1,1)
     var bv = vec3f(0,1,0)
     check(not (av > bv))
     check(not (av < bv))
+  
   test "TExtend":
     var aabb: TAlignedBox3f
     var av = vec3f(5,5,5)
     aabb.extend(av)
     check(aabb.max == av)
+  
   test "TDotProduct":
     var av: TVec3f
     av.data = [1.0'f32, 2.0'f32, 0.0'f32]
@@ -883,35 +994,42 @@ when isMainModule:
     bv.data = [0.0'f32, 5.0'f32, 1.0'f32]
     var cv = dot(av,bv)
     check(cv == 10.0'f32)
+  
   test "TCross1":
     var a = vec3f(1,0,0)
     var b = vec3f(0,1,0)
     var c = cross(a,b)
     check(c == vec3f(0,0,1))
+  
   test "TRow":
     var ta: TMat4f
     ta[1,1] = 1.0'f32
     var tr = ta.row(1)
     check(tr.data == [1'f32, 0'f32, 0'f32, 0'f32])
+  
   test "TColumn":
     var ta: TMat4f
     ta[1,1] = 1.0'f32
     var tc = ta.col(1)
     check(tc.data == [1.0'f32, 0.0'f32, 0.0'f32, 0.0'f32])
+  
   test "TMul":
     var ta: TMat4f
     ta[1,2] = 2.0'f32
     var tb: TMat4f
     tb[2,1] = 2.0'f32
   #  discard mul(ta, tb)
+  
   test "TestMat3f":
     var tm3: TMat3f
     var tm4 = toAffine(tm3)
     check(tm4[4,4] == 1.0'f32)
+  
   test "Test Construct":
     #I actually had a bug where constructors just stopped working
     var vec4: TVec4f = vec4f(1.0'f32, 1.0'f32, 1.0'f32, 1.0'f32)
     check(vec4.data == [1.0'f32, 1.0'f32, 1.0'f32, 1.0'f32])
+  
   test "TSub":
     var a = initMat3f([1'f32,2'f32,3'f32,
                        4'f32,5'f32,6'f32,
@@ -921,16 +1039,19 @@ when isMainModule:
     var b:TMat2f = a.sub(2,2)
     var e:bool = b == c
     check(e)
+  
   test "TDet2x2":
     var a = initMat2f([1'f32,2'f32,3'f32,4'f32])
     var da = det(a)
     check(da == -2'f32)
+  
   test "TDet3x3f":
     var a = initMat3f([1'f32, 2'f32, 3'f32, 
                        4'f32, 5'f32, 6'f32,
                        7'f32, 8'f32, 9'f32])
     var da = det(a)
     check(da == 0.0)
+  
   test "TAdj3x3":
     var a = initMat3f([1'f32, 2'f32, 3'f32, 
                        4'f32, 5'f32, 6'f32,
@@ -941,6 +1062,7 @@ when isMainModule:
                         -3'f32, 6'f32, -3'f32])
     var e = aj == b
     check(e)
+  
   test "TMatToQuat1":
     var rot = quatFromAngleAxis(0.5, vec3f(1,0,0))
     var mtx = toRotMatrix(rot)
